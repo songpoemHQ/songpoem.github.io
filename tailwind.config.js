ca83d1952837f9b44691190bcb7450e659fa1bfc
Sandesh Soni, Scalekrew <sandesh.soni@scalekrew.com>
2024-06-05 01:05:08 +0530

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    './_pages/**/*.html',
+    './_portfolio/**/*.md',
+    './_services/**/*.md',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
