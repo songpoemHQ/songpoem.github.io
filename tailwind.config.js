bda2d7ea40ce3656f61954e01c80d4ab583cd7e9
Sandesh Soni, Scalekrew <sandesh.soni@scalekrew.com>
2024-06-04 19:46:22 +0530

    leverage tailwing css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,13 @@
 module.exports = {
-  purge: [
+  content: [
     './_includes/**/*.html',
     './_layouts/**/*.html',
     './_posts/**/*.md',
-    './index.html',
-    './about.html',
-    './contact.html',
-    // Add more paths as needed
+    './_pages/**/*.md',
+    './index.html'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
