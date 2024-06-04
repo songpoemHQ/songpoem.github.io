883fa5a1b4094c47eaf2d3f4be61a66e716783a6
Sandesh Soni, Scalekrew <sandesh.soni@scalekrew.com>
2024-06-04 19:02:35 +0530

    debug scss css. compile failed for unknown word

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('postcss-import'),
     ...(process.env.JEKYLL_ENV == "production"
       ? [require('cssnano')({})]
       : [])
